ee32b2172b3d303e348ff0a90f85c34b9bbf8448
lohit <lohith.kumar.k.g@gmail.com>
2024-08-04 16:45:20 +0100

    plfundsrisks update

--- taskscheduler.py
+++ taskscheduler.py
@@ -22,34 +22,36 @@ async def interval_task_5mins():
 #     await log_with_bot('i',f"Task-60Mins : {datetime.datetime.now()}")
 async def upate_plfundsrisk():
 
-    algoBroker = AlgoBroker(settings.Broker, settings.UserId)
-    funds_data = await algoBroker.get_funds_data()
-    funds_data = funds_data.get('data', [{}])[0]
+    if settings.Broker == 'Finvasia':
 
-    plFundsRisk_data = PLFundsRiskSchema(
-        Broker=settings.Broker,
-        UserId=settings.UserId,
-        DateTime=str(datetime.datetime.now().replace(microsecond=0)),
-        StartOfTheDayBalance=funds_data.get('AvailableMargin',0.0),
-        AvailableBalance=funds_data.get('AvailableMargin',0.0),
-        UtilizedBalance=2000.0,
-        UtilizationPercentage=20.0,
-        UnrealizedProfit=500.0,
-        RealizedProfit=300.0,
-        PnlAmount=800.0,
-        PnlPercentage=8.0,
-        RiskAmount=200.0,
-        RiskPercentage=2.0
-    )
+        algoBroker = AlgoBroker(settings.Broker, settings.UserId)
+        funds_data = await algoBroker.get_funds_data()
+        funds_data = funds_data.get('data', [{}])[0]
 
-    plFundsRisk = await AlgoPLFundsRisk(settings.Broker, settings.UserId)
-    response = await plFundsRisk.create(plFundsRisk_data)
+        plFundsRisk_data = PLFundsRiskSchema(
+            Broker=settings.Broker,
+            UserId=settings.UserId,
+            DateTime=str(datetime.datetime.now().replace(microsecond=0)),
+            StartOfTheDayBalance=funds_data.get('AvailableMargin',0.0),
+            AvailableBalance=funds_data.get('AvailableMargin',0.0),
+            UtilizedBalance=2000.0,
+            UtilizationPercentage=20.0,
+            UnrealizedProfit=500.0,
+            RealizedProfit=300.0,
+            PnlAmount=800.0,
+            PnlPercentage=8.0,
+            RiskAmount=200.0,
+            RiskPercentage=2.0
+        )
 
-    print(response)
+        plFundsRisk = await AlgoPLFundsRisk(settings.Broker, settings.UserId)
+        response = await plFundsRisk.create(plFundsRisk_data)
+
+        await  log_with_bot('i',f'PLFundsRisks Updated ',str(settings.UserId))
 
 # @TaskScheduler.scheduled_job('cron', hour=15, minute=25)
 # async def cron_task():
 #     await signal_generate_SIP_INV()
 
-# if master_host == '65.2.91.59':
-#     TaskScheduler.add_job(interval_task_5mins, 'interval',  seconds=300, start_date=settings.MarketOpenTime,end_date=settings.MarketCloseTime)
+if master_host == '65.2.91.59':
+    TaskScheduler.add_job(upate_plfundsrisk, 'interval',  seconds=900, start_date=settings.MarketOpenTime,end_date=settings.MarketCloseTime)
